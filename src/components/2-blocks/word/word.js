6b711fd61f31d0c1c24218e8374e9bc2ad04fb28
NoahSekreter <nsekreter@gmail.com>
2025-05-06 10:41:31 -0500

    Updated About Me to match oldschool word style

---
+++ src/components/2-blocks/word/word.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Headshot from './headshot-2025.JPG';
+import './word.scss';
+
+export default function Word() {
+  return (
+    <div className="word">
+      <div className='word__doc'>
+        <h2>About Me</h2>
+        <p>
+          Born in Germany and raised in the states, I am a web developer of 5 years experience
+          with experience in the HTML, CSS, SASS/SCSS, JavaScript, jQuery, C#, and PHP languages to build and 
+          maintain websites, as well as Drupal, Wordpress, .NET, and React as content management systems
+          and development frameworks.
+        </p>
+        <p>
+          Not only that, but I also have experience with various development tools from mySQL, 
+          DDEV, Docker, ESLint, Webpack, Vite, Composer, NodeJS, and Storybook.
+        </p>
+        <p>
+          Additionally, I have some experience with Python as well, which I used for software development,
+          creating distributable software to automate excel-based filing and connecting to a number of RESTful
+          APIs to retrieve data from a number of sources.
+        </p>
+        <p>
+          My hobbies include board games and tabletop roleplaying games such as Dungeons & Dragons and the
+          Warhammer 40k TTRPGs. Not only do I write lots of homebrew, but I even write my own systems 
+          (<a href="https://mega.nz/file/uQo23ZoK#joD8bj81R55vksFhyCURfAgd8kI5LH69ykp_bAmbZYA" rel="noreferrer" target="_blank">
+          Click here to check it out, if you're interested</a>)! I also enjoy playing video games and reading manga in my free time.
+        </p><br />
+        <img src={ Headshot } alt="A headshot of Noah Sekreter"/>
+        <figcaption>A headshot of Noah Sekreter</figcaption>
+      </div>
+    </div>
+  );
+}
--- src/components/3-sections/app/app.js
+++ src/components/3-sections/app/app.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './app.scss';
+import Scroller from '../../1-elements/scroller/scroller.js';
 import Window from '../../2-blocks/window/window.js';
 import EmailCanvas from '../../2-blocks/email-canvas/email-canvas.js';
-import Headshot from './headshot-2025.JPG';
+import Word from '../../2-blocks/word/word.js';
 import Resume from './SekreterResume2025.pdf';
-import Scroller from '../../1-elements/scroller/scroller.js';
-
 
 export default function App() {
   const [isLoaded, setIsLoaded] = useState('');
@@ -155,11 +154,6 @@ export default function App() {
               <a rel="noreferrer" href="https://www.linkedin.com/in/sekreter" target='_blank'>View LinkedIn</a>
               <a rel="noreferrer" href="mailto:nsekreter@gmail.com" target='_blank'>E-Mail me at nsekreter@gmail.com</a>  
             </div>
-            <p><b>New Website! Under construction! Upcoming changes...</b></p>
-            <ul>
-              <li>Prettier Windows!</li>
-              <li>Dark mode!</li>
-            </ul>
           </>
         }
       />
@@ -168,35 +162,8 @@ export default function App() {
         title="About Me"
         id="about-me"
         hidden="true"
-        styles= { { top: '46px', left: '46px' } }
-        content={
-          <>
-            <h2>About Me</h2>
-            <p>
-              Born in Germany and raised in the states, I am a web developer of 5 years experience
-              with experience in the HTML, CSS, SASS/SCSS, JavaScript, jQuery, C#, and PHP languages to build and 
-              maintain websites, as well as Drupal, Wordpress, .NET, and React as content management systems
-              and development frameworks.
-            </p>
-            <p>
-              Not only that, but I also have experience with various development tools from mySQL, 
-              DDEV, Docker, ESLint, Webpack, Vite, Composer, NodeJS, and Storybook.
-            </p>
-            <p>
-              Additionally, I have some experience with Python as well, which I used for software development,
-              creating distributable software to automate excel-based filing and connecting to a number of RESTful
-              APIs to retrieve data from a number of sources.
-            </p>
-            <p>
-              My hobbies include board games and tabletop roleplaying games such as Dungeons & Dragons and the
-              Warhammer 40k TTRPGs. Not only do I write lots of homebrew, but I even write my own systems 
-              (<a href="https://mega.nz/file/uQo23ZoK#joD8bj81R55vksFhyCURfAgd8kI5LH69ykp_bAmbZYA" rel="noreferrer" target="_blank">
-              Click here to check it out, if you're interested</a>)! I also enjoy playing video games and reading manga in my free time.
-            </p>
-            <img src={ Headshot } alt="A headshot of Noah Sekreter"/>
-            <figcaption>A headshot of Noah Sekreter</figcaption>
-          </>
-        }
+        styles= { { top: '46px', left: '46px', height: '500px', width: '800px' } }
+        content={ <Word /> }
       />
 
       <Window
@@ -204,8 +171,7 @@ export default function App() {
         id="work-experience"
         hidden="true"
         styles= { { top: '76px', left: '76px', width: '1000px' } }
-        content={ <EmailCanvas
-          inheritFunc={ createBlueScreen } /> }
+        content={ <EmailCanvas inheritFunc={ createBlueScreen } /> }
       />
 
       <Window
