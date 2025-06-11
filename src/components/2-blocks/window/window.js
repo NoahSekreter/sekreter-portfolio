3a526a04ee1cef59f09720827d709ecb3e5c07de
NoahSekreter <nsekreter@gmail.com>
2025-06-11 11:23:19 -0500

    Added aria-labels to window buttons

--- src/components/2-blocks/window/window.js
+++ src/components/2-blocks/window/window.js
@@ -151,12 +151,14 @@ export default function Window(variables) {
         <span>{ variables.title }</span>
         <button
           onClick={(e) => maxMinWindow(e)}
+          aria-label="Maximize Window"
           className='window__button window__max-min'>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zM96 96l320 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L96 160c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M32 416c-17.7 0-32 14.3-32 32s14.3 32 32 32l448 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 416z"/></svg>
         </button>
         <button
           onClick={(e) => closeWindow(e)}
+          aria-label="Close Window"
           className='window__button window__close'>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
         </button>
